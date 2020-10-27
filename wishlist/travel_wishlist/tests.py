ec54f96f8c153423bebc69e525bb8597eeb5372d
brndn adms <hey@brandenadams.com>
2020-10-27 14:14:03 -0500

    add places tests

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Place
 
-# Create your tests here.
+class TestHomePage(TestCase):
+    def test_load_home_page_shows_empty_list_for_empty_database(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url)
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'You have no places in your wishlist')
+
+
+class TestWishList(TestCase):
+    fixtures = ['test_places']
+    def test_view_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'Tokyo')
+        self.assertContains(response, 'New York')
+        self.assertNotContains(response, 'San Francisco')
+        self.assertNotContains(response, 'Moab')
+
+
+class TestVisitedPage(TestCase):
+    def test_view_visited_contains_not_visited_message(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'You have not visited any places')
+
+
+class TestVisitedPlaces(TestCase):
+    fixtures = ['test_places']
+    def test_view_visitedt_contains_visited_places(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'San Francisco')
+        self.assertContains(response, 'Moab')
+        self.assertNotContains(response, 'Tokyo')
+        self.assertNotContains(response, 'New York')
+
