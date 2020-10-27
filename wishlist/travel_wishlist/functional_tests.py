23d989abfb6c9d9e548ebab39157748ce2e69afc
brndn adms <hey@brandenadams.com>
2020-10-27 14:50:04 -0500

    add functional tests

---
+++ wishlist/travel_wishlist/functional_tests.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.firefox.webdriver import WebDriver
+from django.test import LiveServerTestCase
+
+class TitleTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_title_on_home_page(self):
+        self.selenium.get(self.live_server_url)
+        self.assertIn('Travel Wishlist', self.selenium.title)
