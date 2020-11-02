4dffcadd61a6d6530af8d6ef8f537ae2440a58b8
brndn adms <hey@brandenadams.com>
2020-11-02 10:33:34 -0600

    slide 13 - add user, photo, date visited  field to models.py

--- wishlist/travel_wishlist/models.py
+++ wishlist/travel_wishlist/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Place(models.Model):
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    date_visited = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
 
     def __str__(self):
-        return f'{self.name}, visited? {self.visited}'
+        photo_str = self.photo.url if self.photo else 'no photo'
+        return f'{self.pk}: {self.name}, visited? {self.visited} on {self.date_visited}\nPhoto {photos_str}'
